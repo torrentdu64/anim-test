dd1d1d394bea076ab76ce893b80ecb70a6cd4417
torrent benoit <torrentdu64@gmail.com>
2019-12-09 18:02:16 +1300

    create ball

---
+++ src/Ball.js
@@ -0,0 +1,26 @@
+import React,{ Component } from 'react';
+import { View } from 'react-native';
+
+
+
+class Ball extends Component {
+  render (){
+    return (
+        <View style={styles.ball} />
+      );
+  }
+
+};
+
+const styles = {
+  ball: {
+    height: 60,
+    widht: 60,
+    borderRadius: 30,
+    borderWidth: 30,
+    borderColor: 'black'
+
+  }
+}
+
+export default Ball;
