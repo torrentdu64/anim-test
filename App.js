247f3538f65a8859a6768f2f710e11d955c1eb43
torrent benoit <torrentdu64@gmail.com>
2019-12-12 10:22:25 +1300

    swipe and method default done

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-
 import React from 'react';
 import {
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-
+import { Card, Button } from 'react-native-elements';
 import Deck from './src/Deck';
 
 const DATA = [
@@ -22,18 +21,43 @@ const DATA = [
 class App extends React.Component {
   renderCard(item) {
     return (
-        <Text>{item.text}</Text>
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}>
+          I can customize the Card further.
+        </Text>
+        <Button
+          icon={{ name: 'code' }}
+          backgroundColor="#03A9F4"
+          title="View Now!"
+        />
+      </Card>
     );
   }
 
-
+  renderNoMoreCards() {
+    return (
+      <Card title="All Done!">
+        <Text style={{ marginBottom: 10 }}>
+          There's no more content here!
+        </Text>
+        <Button
+          backgroundColor="#03A9F4"
+          title="Get more!"
+        />
+      </Card>
+    );
+  }
   render() {
     return (
       <View style={styles.container}>
         <Deck
           data={DATA}
           renderCard={this.renderCard}
-
+          renderNoMoreCards={this.renderNoMoreCards}
         />
       </View>
     );
